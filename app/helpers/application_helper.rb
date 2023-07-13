81342ee23c34f2b68f583ade0e89b61ab698adf4
Mateo Ochoa <mateo.ochoac@gmail.com>
2023-07-12 23:50:05 -0700

    Show sections of valid, invalid, accepted, and rejected records only if they are populated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
   def cell_data(import_row, import_header_id)
     import_row.import_cells.where(import_header_id: import_header_id).limit(1).pluck(:raw_data).first
   end
+
+  def show_time_stat(migration, stat)
+    time = migration.statistics_hash[stat]
+    if time.nil?
+      "0 s"
+    else
+      "#{'%.2f' % time} s"
+    end
+  end
 end
